7b93aacf19286cc4c4bf4a0ae7b602058a39d089
dirakiza@caltech.edu <dirakiza@caltech.edu>
2024-05-26 06:33:07 +0000

    Autocommit of demo or game for dirakiza

--- demo/game.c
+++ demo/game.c
@@ -173,7 +173,7 @@ static asset_t *create_btn(btn_element_t btn_element)
 
     asset_t *asset =
         asset_make_button(btn_element.img.frame, img_asset, text_asset, btn_element.handler);
-    asset_cache_register_button(button_asset);
+    asset_cache_register_button(asset);
 
     return button_asset;
 }
