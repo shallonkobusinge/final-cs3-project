520ff1e5f54cb3e9515ae1a070ddea13d8500d78
dirakiza@caltech.edu <dirakiza@caltech.edu>
2024-05-26 07:04:45 +0000

    Autocommit of demo or game for dirakiza

--- demo/game.c
+++ demo/game.c
@@ -107,7 +107,7 @@ text_element_t text_elements[] = {
 img_element_t img_elements[] = {
     {
         .file_path = "assets/images/common/nav_line.png",
-        .frame = (SDL_Rect){0, 400, 100, 100},
+        .frame = (SDL_Rect){(CENTER.x - (280 / 2)), 0, 280, 48},
     },
     {
         .file_path = "assets/images/landing-page/how_to_bg.png",
