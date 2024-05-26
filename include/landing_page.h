7f19cc231f1e4675e30146e441fe388eeb50d8bc
dirakiza@caltech.edu <dirakiza@caltech.edu>
2024-05-26 06:22:41 +0000

    feat: fixing landing page

---
+++ include/landing_page.h
@@ -0,0 +1,15 @@
+#ifndef __LANDING_PAGE_H__
+#define __LANDING_PAGE_H__
+
+typedef struct text_element text_element_t;
+
+typedef struct img_element img_element_t;
+
+typedef struct btn_element btn_element_t;
+
+/**
+ * Build all assets of the landing page
+ */
+void build_landing_page();
+
+#endif // #ifndef __LANDING_PAGE_H__
