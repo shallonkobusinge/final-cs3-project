4103c76c477894e355897a72e2849072223bd978
dirakiza@caltech.edu <dirakiza@caltech.edu>
2024-05-26 07:16:09 +0000

    Autocommit of demo or game for dirakiza

--- demo/game.c
+++ demo/game.c
@@ -80,7 +80,7 @@ text_element_t text_elements[] = {
         .text = "HOW TO PLAY",
         .font_path = "assets/fonts/Inter-Regular.ttf",
         .color = (rgb_color_t){255, 255, 255},
-        .frame = (SDL_Rect){23, 100, 280, 48},
+        .frame = (SDL_Rect){23, 200, 280, 48},
     },
 
     {
