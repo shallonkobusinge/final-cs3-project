bb11622950da3711f2233c0b0ba02e61819e85a0
dirakiza@caltech.edu <dirakiza@caltech.edu>
2024-05-26 07:03:33 +0000

    Autocommit of demo or game for dirakiza

--- demo/game.c
+++ demo/game.c
@@ -74,7 +74,7 @@ text_element_t text_elements[] = {
         .text = "Welcome to The Caltech Hideout",
         .font_path = "assets/fonts/Inter-Regular.ttf",
         .color = (rgb_color_t){0, 0, 0},
-        .frame = (SDL_Rect){CENTER.x, 0, 280, 48},
+        .frame = (SDL_Rect){(CENTER.x - 280), 0, 280, 48},
     },
     {
         .text = "HOW TO PLAY",
