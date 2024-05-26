fd4374bcbb2bbcb7e51cff9a8004afe10d870e0b
dirakiza@caltech.edu <dirakiza@caltech.edu>
2024-05-26 08:42:10 +0000

    Autocommit of library for dirakiza

--- include/landing_page.h
+++ include/landing_page.h
@@ -1,6 +1,8 @@
 #ifndef __LANDING_PAGE_H__
 #define __LANDING_PAGE_H__
 
+#include <stdlib.h>
+
 typedef struct text_element text_element_t;
 
 typedef struct img_element img_element_t;
@@ -13,6 +15,6 @@ typedef struct state state_t;
  * Build all assets of the landing page
  * @return current page
  */
-ssize_t build_landing_page();
+size_t build_landing_page();
 
 #endif // #ifndef __LANDING_PAGE_H__
