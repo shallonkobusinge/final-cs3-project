6a5317afdfbbc121e9037c200934de67769cdb0a
dirakiza@caltech.edu <dirakiza@caltech.edu>
2024-05-26 07:47:10 +0000

    Autocommit of demo or game for dirakiza

--- demo/game.c
+++ demo/game.c
@@ -106,7 +106,7 @@ text_element_t text_elements[] = {
         .text = "2024",
         .font_path = "assets/fonts/Inter-Regular.ttf",
         .color = (rgb_color_t){0, 0, 1},
-        .frame = (SDL_Rect){0, 400, TEXT_FRAME.x, TEXT_FRAME.y},
+        .frame = (SDL_Rect){60, 460, 90, 48},
     },
     {
         .text = "By Shallon & Divin.",
