73b73e2553e7537a6dfccfea0fc35c07245facbe
dirakiza@caltech.edu <dirakiza@caltech.edu>
2024-05-26 01:14:51 +0000

    Autocommit of demo or game for dirakiza

---
+++ demo/game.c
@@ -0,0 +1,12 @@
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "asset.h"
+#include "asset_cache.h"
+#include "collision.h"
+#include "forces.h"
+#include "sdl_wrapper.h"
