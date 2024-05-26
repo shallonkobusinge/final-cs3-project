2d5ad94857627e816b05cda4e7e8175d1d500b0a
dirakiza@caltech.edu <dirakiza@caltech.edu>
2024-05-26 07:25:08 +0000

    Autocommit of demo or game for dirakiza

--- demo/game.c
+++ demo/game.c
@@ -80,7 +80,7 @@ text_element_t text_elements[] = {
         .text = "HOW TO PLAY",
         .font_path = "assets/fonts/Inter-Regular.ttf",
         .color = (rgb_color_t){255, 255, 255},
-        .frame = (SDL_Rect){35, 110, 180, 30},
+        .frame = (SDL_Rect){43, 110, 180, 30},
     },
     {
         .text = "Recycling one aluminum can saves enough energy to power a television for three hours.This is because recycling aluminum saves 95% of the energy required to make the same amount of aluminum from raw materials.",
