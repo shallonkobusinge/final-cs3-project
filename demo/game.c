09256f5bc84683634da0a8ede981f6dd290ae142
skobusin@caltech.edu <skobusin@caltech.edu>
2024-05-27 01:50:59 +0000

    Autocommit of demo or game for skobusin

--- demo/game.c
+++ demo/game.c
@@ -66,7 +66,7 @@ state_t *emscripten_init() {
     // for(int i = 0; i < STARTING_SEEKERS; i++) {
         body_t *seeker = make_seeker(OUTER_RADIUS, INNER_RADIUS, START_POS);
         // vector_t vel = {.x = rand() % 200 - 100, .y = rand() % 200 - 100 };
-        body_set_velocity(seeker, (vector_t){100, 0});
+        // body_set_velocity(seeker, (vector_t){100, 0});
         // body_set_centroid(seeker, START_POS);
         scene_add_body(state->scene, seeker);
         asset_t *asset_seeker = asset_make_image_with_body(SEEKER_PATH, seeker);
