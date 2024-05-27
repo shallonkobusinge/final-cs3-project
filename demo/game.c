5c2e581b91330a3e23822cd7b5fa95c151751f23
skobusin@caltech.edu <skobusin@caltech.edu>
2024-05-27 03:21:51 +0000

    Autocommit of demo or game for skobusin

--- demo/game.c
+++ demo/game.c
@@ -27,9 +27,9 @@ const int16_t H_STEP = 20;
 const int16_t V_STEP = 40;
 const double OUTER_RADIUS = 60;
 const double INNER_RADIUS = 60;
-const double HUMAN_HEAD_RADIUS = 10
-const double BODY_HEIGHT = 30
-const double BODY_WIDTH = 15
+const double HUMAN_HEAD_RADIUS = 10;
+const double BODY_HEIGHT = 30;
+const double BODY_WIDTH = 15;
 
 
 const rgb_color_t seeker_color = (rgb_color_t){0.1, 0.9, 0.2};
@@ -66,7 +66,7 @@ list_t *rect_shape(double width, double height, vector_t center) {
 }
 
 body_t *make_seeker(vector_t center){
-    list_t *seeker_p = rect_shape(2 * HUMAN_RADIUS, 2 * HUMAN_RADIUS, (vector_t){center.x, center.y + BODY_HEIGHT / 2 + HUMAN_HEAD_RADIUS})
+    list_t *seeker_p = rect_shape(2 * HUMAN_RADIUS, 2 * HUMAN_RADIUS, (vector_t){center.x, center.y + BODY_HEIGHT / 2 + HUMAN_HEAD_RADIUS});
   body_t *seeker = body_init(seeker_p, 1, seeker_color);
   body_set_centroid(seeker, center);
   return seeker
