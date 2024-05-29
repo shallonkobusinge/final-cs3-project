19fa124ae1d1172377f049c7c1f749d2cd6946b8
dirakiza@caltech.edu <dirakiza@caltech.edu>
2024-05-29 04:29:47 +0000

    Autocommit of demo or game for dirakiza

--- demo/game.c
+++ demo/game.c
@@ -18,9 +18,6 @@ const vector_t CENTER = {500, 250};
 #define HEIGHT 800
 #define CELL_SIZE 20
 
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
-
 typedef struct
 {
     int x, y;
@@ -33,13 +30,9 @@ struct state
 
 void draw_maze()
 {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderClear(renderer);
+    // Draw the maze walls (hardcoded example).
+    // You can replace this with your maze generation algorithm.
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-
-    // Draw the maze walls (hardcoded example)
-    // You can replace this with your maze generation algorithm
     int maze[20][20] = {/* Your maze array here */};
 
     for (int i = 0; i < 20; ++i)
@@ -48,13 +41,10 @@ void draw_maze()
         {
             if (maze[i][j] == 1)
             {
-                SDL_Rect rect = {j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE};
-                SDL_RenderFillRect(renderer, &rect);
-            }
+                SDL_Rect rect = {j * 200, i * 200, 200, 200};
+                        }
         }
     }
-
-    SDL_RenderPresent(renderer);
 }
 
 state_t *emscripten_init()
