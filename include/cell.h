710ac297beee83f69ed682fd16738afb07557d68
dirakiza <dirakiza@caltech.edu>
2024-05-31 09:17:14 +0000

    Autocommit of library for dirakiza

--- include/cell.h
+++ include/cell.h
@@ -20,7 +20,7 @@ typedef struct cell
  * @param adj_matrix adjancey matrix with direct connections
  *
  */
-void adjacency(cell_t *current, cell_t *neighbor, bool adj_matrix[][NUM_CELLS]);
+void adjacency(cell_t *current, cell_t *neighbor, bool adj_matrix[][CELLS]);
 /**
  *
  * This function checks if there is a direct connection (adjacency) between two cells, Cell and cellNeighbour.
@@ -29,13 +29,13 @@ void adjacency(cell_t *current, cell_t *neighbor, bool adj_matrix[][NUM_CELLS]);
  * @param adj_matrix adjancey matrix with direct connections
  * @return true if there is a direct connection between current and neighbor.
  */
-bool isAdjacency(cell_t *current, cell_t *neighbor, bool adj_matrix[][NUM_CELLS]);
+bool isAdjacency(cell_t *current, cell_t *neighbor, bool adj_matrix[][CELLS]);
 
 /**
  * Finds a random unvisited neighbor of the current cell.
  * @param cell current cell.
  * @returns NULL if all neighbors are visited. Otherwise, it returns a random unvisited neighbor.
  */
-cell_t *get_neighbor(cell_t *current, bool visited[][GRID_HEIGHT + 2]);
+cell_t *get_neighbor(cell_t *current, bool visited[][HEIGHT + 2]);
 
 #endif // #ifndef __CELL_H__
