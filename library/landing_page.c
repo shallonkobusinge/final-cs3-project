08ecc6cf2a981f9a1844f8842646bed7bfaabd41
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-01 06:45:42 +0000

    Autocommit of library for skobusin

--- library/landing_page.c
+++ library/landing_page.c
@@ -5,7 +5,7 @@
 const vector_t SCREEN_MAX = {1000, 500};
 const vector_t SCREEN_CENTER = {500, 250};
 
-const size_t LANDING_PAGE_IMG_ELEMENTS = 5;
+const size_t LANDING_PAGE_IMG_ELEMENTS = 4;
 const size_t LANDING_PAGE_TEXT_ELEMENTS = 7;
 const size_t LANDING_PAGE_BTN_ELEMENTS = 1;
 
