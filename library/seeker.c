c337df1f167a9261f7d47ac593c665d71d6fbe27
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-04 18:33:24 +0000

    Autocommit of demo or game for skobusin

--- library/seeker.c
+++ library/seeker.c
@@ -75,8 +75,8 @@ void wrap_seeker_scene(body_t *seeker) {
 
 
 void add_new_seeker(scene_t *scene, seeker_t *seeker_ipt, bool is_new){
-   
-   vector_t seeker_vel = {.x = 0.0, .y = 0.0};
+    printf(" WE are heree ");
+  //  vector_t seeker_vel = {.x = 0.0, .y = 0.0};
    body_t *seeker;
     // if(is_new){
     //   vector_t seeker_pos = (vector_t){
@@ -90,10 +90,11 @@ void add_new_seeker(scene_t *scene, seeker_t *seeker_ipt, bool is_new){
     //   seeker = make_seeker(OUTER_RADIUS, INNER_RADIUS, seeker_pos);
     // }
     seeker = make_seeker(OUTER_RADIUS, INNER_RADIUS, START_POS);
-    seeker_vel = INITIAL_VELOCITY;
+    // seeker_vel = INITIAL_VELOCITY;
+    printf(" BODY CENTROID: X: %zu Y: %zu \n", seeker->centroid.x, seeker->centroid.y);
   
     scene_add_body(scene, seeker);
-    body_set_velocity(seeker, seeker_vel);
+    // body_set_velocity(seeker, seeker_vel);
     asset_t *new_asset_seeker = asset_make_image_with_body(SEEKER_PATH, seeker);
     list_add(seeker_ipt->body_assets, new_asset_seeker);
     seeker_ipt->last_seeker_time = 0;
@@ -103,10 +104,10 @@ void add_new_seeker(scene_t *scene, seeker_t *seeker_ipt, bool is_new){
 void introduce_seeker(scene_t *scene, seeker_t *seeker, double dt, sound_effect_t *sound_effect){
     printf(" WE are heree ");
     seeker->last_seeker_time += dt;
-    if(seeker->last_seeker_time >= NEW_SEEKERS_INTERVAL){
+    // if(seeker->last_seeker_time >= NEW_SEEKERS_INTERVAL){
       add_new_seeker(scene, seeker, true);
-       tagged_sound(sound_effect);
-    }
+      //  tagged_sound(sound_effect);
+    // }
     for (size_t i = 0; i < list_size(seeker->body_assets); i++) {
       asset_render(list_get(seeker->body_assets, i));
     }
