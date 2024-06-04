81691189ad8759f5140329ef7ae7367bbb5d74b7
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-04 05:38:45 +0000

    Autocommit of library for skobusin

--- library/generate_maze.c
+++ library/generate_maze.c
@@ -33,7 +33,7 @@ typedef struct state
 } state_t;
 
 typedef struct seeker {
-    SDL_Rect *rect;
+    SDL_Rect rect;
 }seeker_t;
 /**
  * Finds max between two numbers
@@ -97,9 +97,9 @@ static size_t find_min(size_t a, size_t b)
 
 static void render_seeker(){
     seeker_t *seeker = malloc(sizeof(seeker_t));
-    seeker->rect = (SDL_Rect){(((GRID_WIDTH - 5) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4), (((GRID_HEIGHT - 5) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4), (GRID_CELL_SIZE / 2), (GRID_CELL_SIZE / 2) };
+    seeker.rect = (SDL_Rect){(((GRID_WIDTH - 5) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4), (((GRID_HEIGHT - 5) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4), (GRID_CELL_SIZE / 2), (GRID_CELL_SIZE / 2) };
     render_color((rgb_color_t){0, 0, 0});
-    render_rect(seeker->rect);
+    render_rect(&seeker->rect);
 }
 
 /**
