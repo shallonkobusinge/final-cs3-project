d7eca712a2d1bbdcd00e170296ab15bc854eb0ca
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-04 05:42:01 +0000

    Autocommit of library for skobusin

--- library/generate_maze.c
+++ library/generate_maze.c
@@ -97,9 +97,11 @@ static size_t find_min(size_t a, size_t b)
 
 static void render_seeker(){
     seeker_t *seeker = malloc(sizeof(seeker_t));
-    seeker->rect = (SDL_Rect){(((GRID_WIDTH - 5) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4), (((GRID_HEIGHT - 5) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4), (GRID_CELL_SIZE / 2), (GRID_CELL_SIZE / 2) };
-    render_color((rgb_color_t){0, 0, 0});
-    render_rect(&seeker->rect);
+    for(int i =0; i < 20; i++) {
+        seeker[i]->rect = (SDL_Rect){(((rand() % GRID_WIDTH) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4), (((rand() % GRID_HEIGHT) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4), (GRID_CELL_SIZE / 2), (GRID_CELL_SIZE / 2) };
+        render_color((rgb_color_t){0, 0, 0});
+        render_rect(&seeker[i]->rect);
+    }
 }
 
 /**
