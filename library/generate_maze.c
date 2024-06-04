aca23016c4799ea180284e21abbcc8f818c7afef
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-04 09:35:04 +0000

    move: single rect

--- library/generate_maze.c
+++ library/generate_maze.c
@@ -59,7 +59,7 @@ static size_t find_min(size_t a, size_t b)
 */
 static void random_move_seeker () {
     printf("WE are here ");
-    SDL_Delay(100);
+    SDL_Delay(70);
     int direction = rand() % 4;
     switch (direction) {
     case 0: { // move left
