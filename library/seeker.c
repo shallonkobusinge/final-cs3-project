a3ac06e6b7a52168ff5d8d1cba0f76bee6c6f004
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-05 03:33:40 +0000

    Autocommit of library for skobusin

--- library/seeker.c
+++ library/seeker.c
@@ -58,7 +58,7 @@ body_t *make_seeker(double w, double h, vector_t center) {
   // list_add(c, v4);
   // body_t *seeker = body_init(c, 6, seeker_color);
    vector_t *v1 = malloc(sizeof(vector_t));
-  *v1 = (vector_t){center.x + w , center.y + h};
+  *v1 = (vector_t){0 , 0};
   list_add(c, v1);
 
   vector_t *v2 = malloc(sizeof(vector_t));
