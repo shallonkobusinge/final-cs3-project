f88f64ea48423c5a1d9479bd4427241cd8a843d5
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-05 05:49:39 +0000

    Autocommit of demo or game for skobusin

--- demo/game.c
+++ demo/game.c
@@ -42,11 +42,11 @@ state_t *emscripten_init()
     state_t *state = malloc(sizeof(state_t));
     state->scene = scene_init();
     state->maze_generated = false;
-    state->sound_effect = load_game_sounds();
+    // state->sound_effect = load_game_sounds();
     state->seeker = seeker_init(state);
     add_new_seeker(state, false);
     state->page = 1;
-    game_sound(state->sound_effect);
+    // game_sound(state->sound_effect);
 
     return state;
 }
