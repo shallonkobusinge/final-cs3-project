521e747f035b040ccb810fa00812a6c199fe15a4
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-05 03:22:17 +0000

    Autocommit of demo or game for skobusin

--- demo/game.c
+++ demo/game.c
@@ -71,8 +71,8 @@ bool emscripten_main(state_t *state)
         // random_move_seeker(state->scene);
         for(size_t i = 0; i < scene_bodies(state->scene); i++) {
             body_t *seeker = scene_get_body(state->scene, i);
-            rgb_color_t color = body_get_color(seeker);
-            if(color.r == 0.1 && color.g == 0.9 && color.b == 0.2) {
+            rgb_color_t *color = body_get_color(seeker);
+            if(color->r == 0.1 && color->g == 0.9 && color->b == 0.2) {
                 random_move_seeker(seeker);
                 body_tick(seeker, dt);
 
