c21b2952114c44e02d60568e3150516f730e94ed
dirakiza <dirakiza@caltech.edu>
2024-06-07 21:44:58 +0000

    Autocommit of library for dirakiza

--- include/stack.h
+++ include/stack.h
@@ -4,19 +4,26 @@
 #include "cell.h"
 
 typedef struct stack stack_t;
+typedef struct node_t Node;
 
 /**
  * Add a new cell to the start of the stack
  * @param head stack to push to
  * @param cell cell to push to stack
  */
-void push_stack(stack_t **head, cell_t *cell);
+// void push_stack(stack_t **head, cell_t *cell);
 
 /**
  * Remove a cell at the start of the stack
  * @param head stack to pop from
  * @param cell cell to pop from stack
  */
-cell_t *pop_stack(stack_t **head);
+// cell_t *pop_stack(stack_t **head);
 
-#endif // #ifndef __STACK_H__
+void push(Node **stack, size_t x, size_t y);
+
+void pop(Node **stack, int *x, int *y);
+
+bool is_empty(Node *stack);
+
+#ifndef __STACK_H__
