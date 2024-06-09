d47c83b97ff89369217d2a23536791cb221b83cf
dirakiza <dirakiza@caltech.edu>
2024-06-09 23:40:45 +0000

    Autocommit of library for dirakiza

--- library/seeker.c
+++ library/seeker.c
@@ -147,7 +147,7 @@ seeker_t *seeker_init(state_t *state)
 {
   seeker_t *seeker = malloc(sizeof(seeker_t));
   seeker->last_seeker_time = 0;
-  // hider_init(state);
+  hider_init(state);
   printf(" PAGE %zu \n", state->page);
   // add_new_seeker(state, false);
   // printf(" SIZE %zu \n", list_size(state->body_assets));
