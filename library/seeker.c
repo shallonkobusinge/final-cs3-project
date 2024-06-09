489fd4ac378867355e14b7f9fbaa725479611ba7
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-09 12:21:35 +0000

    Autocommit of library for skobusin

--- library/seeker.c
+++ library/seeker.c
@@ -25,7 +25,7 @@ extern const int MAZE_WINDOW_HEIGHT;
 // SEEKING CONSTANTS
 const int S_NUM_POINTS = 20;
 const double S_RADIUS = 0.1;
-const int NEW_SEEKERS_INTERVAL = 60;
+const int NEW_SEEKERS_INTERVAL = 3;
 
 const rgb_color_t SEEKER_COLOR = (rgb_color_t){0.1, 0.9, 0.2};
 
