3098f5a5e68bd61179d6f0d339dce1bf772e58e4
dirakiza <dirakiza@caltech.edu>
2024-06-09 09:52:39 +0000

    Autocommit of library for dirakiza

--- library/seeker.c
+++ library/seeker.c
@@ -240,7 +240,8 @@ void seekers_random_movement(state_t *state)
 static void end_game(body_t *body1, body_t *body2, vector_t axis, void *aux,
                      double force_const)
 {
-  printf("GAME OVER: %s\n", (state_t *)aux->page);
+  state_t *state = aux;
+  printf("GAME OVER: %s\n", state->page);
 }
 
 void seeker_collision(state_t *state)
