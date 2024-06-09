51411a34d78ff3ebbded0add6c4091e4a2cffcac
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-09 06:39:55 +0000

    Autocommit of library for skobusin

--- library/maze.c
+++ library/maze.c
@@ -84,8 +84,9 @@ vector_t traverse_maze(state_t *state, vector_t new_vec) {
     };
     for(size_t y = 0; y < GRID_HEIGHT; y++) {
         for(size_t x = 0; x < GRID_WIDTH; x++) {
-       
-           if((maze->cells[y][x].box.x == (int)vec.x) && (maze->cells[y][x].box.y == (int)vec.y)) {
+         if((maze->cells[y][x].box.x == (int)vec.x) && (maze->cells[y][x].box.y == (int)vec.y)) {
+                printf(" MAZE x = %d y = %d VECTOR x = %d y = %d \n", maze->cells[y][x].box.x, maze->cells[y][x].box.y, (int)vec.x, (int)vec.y);
+                printf("NORTH = %d SOUTH = %d EAST = %d WEST = %d  \n", maze->cells[y][x].north, maze->cells[y][x].south, maze->cells[y][x].east, maze->cells[y][x].west);
                 if(!maze->cells[y][x].north) {
                      valid_move = (vector_t){.x = 0, .y = -GRID_CELL_SIZE};
                      break;
@@ -105,7 +106,6 @@ vector_t traverse_maze(state_t *state, vector_t new_vec) {
                      break;
                  }
             }
-        }
     }
     // printf("THE VECTOR x = %f y = %f \n", valid_move.x, valid_move.y);
     return valid_move;
