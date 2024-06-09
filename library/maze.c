00b521f52764399924cbed97f7c27ebb31513e83
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-09 06:52:57 +0000

    Autocommit of library for skobusin

--- library/maze.c
+++ library/maze.c
@@ -384,7 +384,7 @@ void show_maze(state_t *state, double dt)
 
     init_grid(state);
     draw_maze(state->maze_state->maze);
-    seekers_random_movement(state);
+    // seekers_random_movement(state);
     // render_another_seeker(state, dt);
     render_bodies(state->body_assets);
     // seeker_collision(state);
