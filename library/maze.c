1430b5b738c18584513b0ff17a5ba5794fdc8ae0
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-09 06:00:18 +0000

    Autocommit of library for skobusin

--- library/maze.c
+++ library/maze.c
@@ -82,7 +82,7 @@ vector_t traverse_maze(state_t *state, vector_t vec) {
     for(size_t y = 0; y < GRID_HEIGHT; y++) {
         for(size_t x = 0; x < GRID_WIDTH; x++) {
         //    if((maze->cells[y][x].box.x == (int)vec.x) && (maze->cells[y][x].box.y == (int)vec.y)) {
-             printf(" MAZE x = %d y = %d VECTOR x = %d y = %d \n", maze->cells[y][x].box.x, maze->cells[y][x].box.y, (int)(((GRID_WIDTH - 2))), (int)(GRID_HEIGHT - 6));
+             printf(" MAZE x = %d y = %d VECTOR x = %d y = %d \n", maze->cells[y][x].box.x, maze->cells[y][x].box.y, (int)(((GRID_WIDTH - 2) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4), (int)(((GRID_HEIGHT - 6) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4));
             
         //         printf("NORTH = %d SOUTH = %d EAST = %d WEST = %d  \n", maze->cells[y][x].north, maze->cells[y][x].south, maze->cells[y][x].east, maze->cells[y][x].west);
         //         if(maze->cells[y][x].north == true) {
