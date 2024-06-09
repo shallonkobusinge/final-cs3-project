ab3799aeae83fedd7d2eaf69a76535a2715f0237
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-09 13:37:02 +0000

    Autocommit of library for skobusin

--- library/maze.c
+++ library/maze.c
@@ -13,7 +13,7 @@
 #include "seeker.h"
 #include "asset.h"
 
-const size_t GRID_WIDTH = 30;
+const size_t GRID_WIDTH = 40;
 const size_t GRID_HEIGHT = 15;
 const size_t NUM_CELLS = GRID_WIDTH * GRID_HEIGHT;
 const size_t GRID_CELL_SIZE = 40;
