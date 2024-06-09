5636eace8b743362b9edd54ab483574557ae7c27
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-09 11:44:27 +0000

    Autocommit of library for skobusin

--- library/seeker.c
+++ library/seeker.c
@@ -96,8 +96,8 @@ static void add_new_seeker(state_t *state, bool is_new)
   else
   {
     vector_t center = (vector_t){.x = (((GRID_WIDTH - 2) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 2),
-                                 .y = (((GRID_HEIGHT - 6) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 2)};
-    seeker = make_body(GRID_CELL_SIZE / 2, GRID_CELL_SIZE / 2, center, SEEKER_COLOR);
+                                 .y = (((GRID_HEIGHT - 6) * GRID_CELL_SIZE) - GRID_CELL_SIZE / 2)};
+    seeker = make_body(GRID_CELL_SIZE, GRID_CELL_SIZE, center, SEEKER_COLOR);
   }
 
   scene_add_body(state->scene, seeker);
