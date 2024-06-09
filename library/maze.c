f4a649805eb807eafc8e8987413018f3d6d1bff2
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-09 06:15:43 +0000

    Autocommit of library for skobusin

--- library/maze.c
+++ library/maze.c
@@ -79,11 +79,13 @@ vector_t traverse_maze(state_t *state, vector_t vec) {
     maze_t *maze = state->maze_state->maze;
     //  (vector_t){.x = (((GRID_WIDTH - 2) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4), 
                                     // .y = (((GRID_HEIGHT - 6) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4)};
+            
     for(size_t y = 0; y < GRID_HEIGHT; y++) {
         for(size_t x = 0; x < GRID_WIDTH; x++) {
+             printf(" MAZE x = %d y = %d VECTOR x = %d y = %d \n", maze->cells[y][x].box.x, maze->cells[y][x].box.y, (int)(((GRID_WIDTH * 2) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4), (int)(((GRID_HEIGHT - 6) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4));
+    
            if((maze->cells[y][x].box.x == (int)vec.x) && (maze->cells[y][x].box.y == (int)vec.y)) {
-            //  printf(" MAZE x = %d y = %d VECTOR x = %d y = %d \n", maze->cells[y][x].box.x, maze->cells[y][x].box.y, (int)(((GRID_WIDTH * 2) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4), (int)(((GRID_HEIGHT - 6) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 4));
-            
+
                 printf("NORTH = %d SOUTH = %d EAST = %d WEST = %d  \n", maze->cells[y][x].north, maze->cells[y][x].south, maze->cells[y][x].east, maze->cells[y][x].west);
         //         if(maze->cells[y][x].north == true) {
         //             valid_move = (vector_t){.x = 0, .y = -GRID_CELL_SIZE};
