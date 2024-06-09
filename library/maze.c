8a59caac07e784acc8f33352a93d1fc0518c3835
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-09 05:46:33 +0000

    Autocommit of library for skobusin

--- library/maze.c
+++ library/maze.c
@@ -77,8 +77,8 @@ maze_t *create_maze()
 vector_t traverse_maze(state_t *state, vector_t vec) {
     vector_t valid_move = VEC_ZERO;
     maze_t *maze = state->maze_state->maze;
-    for(size_t y = 0; y < GRID_HEIGHT; y++) {
         for(size_t x = 0; x < GRID_WIDTH; x++) {
+            for(size_t y = 0; y < GRID_HEIGHT; y++) {
            if((maze->cells[x][y].box.x == (int)vec.x) && (maze->cells[x][y].box.y == (int)vec.y)) {
             //  printf(" MAZE x = %d y = %d VECTOR x = %d y = %d \n", maze->cells[x][x].box.x, maze->cells[y][x].box.y, (int)vec.x, (int)vec.y);
                 printf("NORTH = %d SOUTH = %d EAST = %d WEST = %d  \n", maze->cells[y][x].north, maze->cells[y][x].south, maze->cells[y][x].east, maze->cells[y][x].west);
