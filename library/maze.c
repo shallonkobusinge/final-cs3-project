5f4e994ee8df0070302d0c2437b2cdf7cdfad021
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-09 05:22:53 +0000

    Autocommit of library for skobusin

--- library/maze.c
+++ library/maze.c
@@ -75,29 +75,34 @@ maze_t *create_maze()
  * Traverse the maze
 */
 vector_t traverse_maze(state_t *state, vector_t vec) {
-    // vector_t valid_move = VEC_ZERO;
+    vector_t valid_move = VEC_ZERO;
     maze_t *maze = state->maze_state->maze;
     for(size_t y = 0; y < GRID_HEIGHT; y++) {
         for(size_t x = 0; x < GRID_WIDTH; x++) {
             if((maze->cells[y][x].box.x == (int)vec.x) && (maze->cells[y][x].box.y == (int)vec.y)) {
                 if(maze->cells[y][x].north == true) {
-                    return (vector_t){.x = 0, .y = -GRID_CELL_SIZE};
+                    valid_move = (vector_t){.x = 0, .y = -GRID_CELL_SIZE};
+                    break;
                 }
                 if(maze->cells[y][x].east == true) {
-                    return (vector_t){.x = GRID_CELL_SIZE, .y = 0};
+                    valid_move = (vector_t){.x = GRID_CELL_SIZE, .y = 0};
+                    break;
                 }
 
                 if(maze->cells[y][x].west == true) {
-                    return (vector_t){.x = -GRID_CELL_SIZE, .y = 0};
+                    valid_move = (vector_t){.x = -GRID_CELL_SIZE, .y = 0};
+                    break;
                 }
 
                 if(maze->cells[y][x].south == true) {
-                    return (vector_t){.x = GRID_CELL_SIZE, .y = 0};
+                    valid_move = (vector_t){.x = GRID_CELL_SIZE, .y = 0};
+                    break;
                 }
             }
         }
     }
-    return VEC_ZERO;
+    printf("THE VECTOR x = %f y = %f \n", valid_move.x, valid_move.y);
+    return valid_move;
 }
 
 /**
