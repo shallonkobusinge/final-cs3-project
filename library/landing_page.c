899cad9b0d2aee4f17734e1416ad320b9e31e244
dirakiza <dirakiza@caltech.edu>
2024-06-10 01:32:11 +0000

    Autocommit of library for dirakiza

--- library/landing_page.c
+++ library/landing_page.c
@@ -144,12 +144,12 @@ static btn_element_t end_btn_elements[] = {
         .handler = (void *)load_game_screen,
     },
     {
-        .text.frame = (SDL_Rect){SCREEN_CENTER.x - 20, SCREEN_CENTER.y, 90, 48},
+        .text.frame = (SDL_Rect){SCREEN_CENTER.x - 65, SCREEN_CENTER.y + 30, 90, 48},
         .text.font_path = "assets/fonts/Inter-Regular.ttf",
         .text.color = (rgb_color_t){0, 0, 0},
-        .text.text = "Go to Main Screen",
-        .img.file_path = "assets/images/landing-page/play_btn.png",
-        .img.frame = (SDL_Rect){SCREEN_CENTER.x - 65, SCREEN_CENTER.y + 50, 200, 80},
+        .text.text = "Go to Home",
+        .img.file_path = "assets/images/landing-page/white_btn.png",
+        .img.frame = (SDL_Rect){SCREEN_CENTER.x - 80, SCREEN_CENTER.y + 50, 200, 80},
         .handler = (void *)load_game_screen,
     },
 };
