d5612253df6176f83d7273519e69072f0a71a204
dirakiza <dirakiza@caltech.edu>
2024-06-10 04:43:38 +0000

    Autocommit of library for dirakiza

--- library/maze.c
+++ library/maze.c
@@ -268,7 +268,7 @@ static void buildings_init(maze_state_t *maze_state)
         do
         {
             random_color = color_get_random();
-        } while (color->r == 0 && color->g == 0 && color->b == 0);
+        } while (colrandom_coloror->r == 0 && random_color->g == 0 && random_color->b == 0);
 
         maze_state->buildings[i] = (building_t){
             .x = ((GRID_WIDTH - rand_x) * GRID_CELL_SIZE) + GRID_CELL_SIZE / 2,
