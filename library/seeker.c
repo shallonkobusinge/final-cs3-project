0baea56817d433d74746067ba8d2017b5aed7851
skobusin@caltech.edu <skobusin@caltech.edu>
2024-06-10 05:11:37 +0000

    Autocommit of library for skobusin

--- library/seeker.c
+++ library/seeker.c
@@ -155,7 +155,7 @@ void render_seeker(state_t *state, double dt)
  */
 static void hider_init(state_t *state)
 {
-  vector_t center = (vector_t){.y = (((GRID_HEIGHT - 10) * GRID_CELL_SIZE) - (GRID_CELL_SIZE / 10)),
+  vector_t center = (vector_t){.y = (((GRID_HEIGHT - 10) * GRID_CELL_SIZE) - (GRID_CELL_SIZE / 2)),
                                .x = (((GRID_WIDTH - 10) * GRID_CELL_SIZE) + (GRID_CELL_SIZE) / 2)};
   add_to_scene(state, center, (rgb_color_t){50, 129, 110}, BEAVER_PATH);
 }
