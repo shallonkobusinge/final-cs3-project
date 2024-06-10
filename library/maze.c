51aca6ce74a777ade5bb297622a6dfdb281a38a3
dirakiza <dirakiza@caltech.edu>
2024-06-10 06:02:15 +0000

    Autocommit of library for dirakiza

--- library/maze.c
+++ library/maze.c
@@ -136,7 +136,7 @@ static void init_grid(state_t *state)
     for (size_t i = 0; i < NUM_BUILDINGS; i++)
     {
         vector_t center = (vector_t){.x = maze_state->buildings[i].x, .y = maze_state->buildings[i].y};
-        add_to_scene(state, center, *maze_state->buildings[i].color, building_paths[i]);
+        add_to_scene(state, center, (rgb_color_t){200, 200, 200}, building_paths[i]);
     }
 }
 
